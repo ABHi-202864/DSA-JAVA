5032ad61bf7ea09b84577dbb9dd4f12e52120918
ABHI <abhi202864@gmail.com>
2024-07-29 23:55:49 +0530

    add ejs, socket.io

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.send("i'm root");
+});
+
+server.listen(3000, () => console.log("doking on - 3000"));
