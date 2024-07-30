9a80981aaed4ddf93c4bd011cdff3326623ea650
ABHI <abhi202864@gmail.com>
2024-07-31 01:36:46 +0530

    fixed app.set() to app.use() middlewere

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    console.log("connected");
+});
 
 app.get("/", (req, res) => {
-    res.send("i'm root");
+    res.render("index");
 });
 
 
-// _________________
-app.get("/show", (req, res, next) => {
-    res.send("im root");
-});
+
 
 server.listen(3000, () => console.log("doking on - 3000"));
