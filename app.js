a01d0032d8dadf7c840c83dd9d6c3179b221ca0f
ABHI <abhi202864@gmail.com>
2024-07-30 23:21:50 +0530

    add show

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.get("/", (req, res) => {
     res.send("i'm root");
 });
 
+
+// _________________
+app.get("/show", (req, res, next) => {
+    res.send("im root");
+});
+
 server.listen(3000, () => console.log("doking on - 3000"));
