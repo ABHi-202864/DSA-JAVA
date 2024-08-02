9d8bc5a4ff5090c180da65b3907c369180363048
ABHI <abhi202864@gmail.com>
2024-08-02 23:39:31 +0530

    add map 1st part

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,28 @@
 const socket = io();
 
-console.log("hey");
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (err) => {
+            console.error(err);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+}
+
+const map = L.map("map").setView([0, 0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "OpenStreetMap"
+    }
+).addTo(map);
+
+const markers = {};
