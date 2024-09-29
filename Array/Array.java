07a7074a1280b3f45e1358ff4ba23a2cd5590186
ABHI <abhi202864@gmail.com>
2024-09-29 23:25:08 +0530

    add Array 1st Part

--- Array/Array.java
+++ Array/Array.java
@@ -1,6 +1,7 @@
-package array;
+package array; // Declare PACKAGE
 
 import java.util.*;
+// import function.Function; // importing class from other packages 
 
 public class Array {
 
@@ -26,6 +27,7 @@ public class Array {
     return -1;
   }
 
+  // LEARGEST IN ARRAY
   public static int largestInArray(int[] n) {
     int largest = Integer.MIN_VALUE;
     // int smallest = Integer.MAX_VALUE;
@@ -37,6 +39,95 @@ public class Array {
     }
 
     return largest;
+  }
+
+  // BINARY SEARCH
+  public static int binarySearch(int[] number, int key) {
+    int start = 0, end = number.length - 1;
+
+    while (start <= end) {
+      int mid = (start + end) / 2;
+      if (number[mid] == key) {
+        return mid;
+      }
+
+      if (number[mid] < key) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+
+    return -1;
+  }
+
+  // REVERSE IN ARRAY
+  public static void reverse(int[] numbers) {
+    int first = 0, last = numbers.length - 1;
+
+    while (first < last) {
+      int temp = numbers[first];
+      numbers[first] = numbers[last];
+      numbers[last] = temp;
+
+      first++;
+      last--;
+    }
+  }
+
+  // PAIRS IN ARRAY
+  public static void printPairs(int[] arr) {
+    int tp = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        System.out.print("(" + arr[i] + "," + arr[j] + ") ");
+        tp++;
+      }
+      System.out.println();
+    }
+
+    System.out.println("tp = " + tp);
+  }
+
+  // SUBARRAY
+  public static void printSubarray(int[] arr) {
+    int ts = 0;
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        for (int k = i; k <= j; k++) {
+          System.out.print(arr[k] + " ");
+        }
+        ts++;
+        System.out.println();
+      }
+      System.out.println();
+    }
+
+    System.out.println("ts : " + ts);
+  }
+
+  // MAX SUBARRAY SUM -- (brute force)
+  public static int maxSubarraySum(int[] arr) {
+    int MS = Integer.MIN_VALUE;
+    int CS = 0;
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length; j++) {
+        CS = 0;
+        for (int k = i; k <= j; k++) {
+          CS += arr[k];
+        }
+        System.out.print(CS + " ");
+        if (MS < CS) {
+          MS = CS;
+        }
+      }
+      System.out.println();
+    }
+    System.out.println("MAX SUN = " + MS);
+
+    return MS;
   }
 
   public static void main(String[] args) {
@@ -51,9 +142,33 @@ public class Array {
     // String userNeed = sc.nextLine();
     // System.out.println(foodSearch(food, userNeed));
 
-    // ---- BIG NB SEARCH
-    int[] n = { 1, 2, 6, 3, 5 };
-    System.out.println("Leargst Number : " + largestInArray(n));
+    // ---- LARGEST IN ARRAY
+    // int[] n = { 1, 2, 6, 3, 5 };
+    // System.out.println("Leargst Number : " + largestInArray(n));
+
+    // ---- BINARY SEARCH
+    // int[] number = { 2, 4, 6, 8, 10, 12, 14, 16 };
+    // System.out.println(binarySearch(number, 10));
+    // }
+
+    // // ---- REVERSE IN ARRAY
+    // int[] numbers = { 2, 4, 6, 8, 10 };
+    // reverse(numbers);
+    // for (int i = 0; i < numbers.length; i++) {
+    // System.out.print(numbers[i] + " ");
+    // }
+
+    // ----- PAIRS IN ARRAY
+    // int[] arr = { 2, 4, 6, 8, 10 };
+    // printPairs(arr);
+
+    // ----- PRINT SUBARRAY
+    // int[] arr = { 2, 4, 6, 8, 10 };
+    // printSubarray(arr);
+
+    // MAX SUBARRAY SUM -- (brute force)
+    // int[] arr = { 1, -2, 6, -1, 3 };
+    // maxSubarraySum(arr);
 
   }
 }
